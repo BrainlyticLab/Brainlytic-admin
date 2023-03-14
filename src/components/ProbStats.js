99e38a7cb10cb1b27c60e6d2953e88e80a6bd9cd
rabib-jahin <rabibjahin2000@gmail.com>
2023-03-15 00:27:13 +0600

    updated

--- src/components/ProbStats.js
+++ src/components/ProbStats.js
@@ -148,7 +148,28 @@ document.getElementById("4").classList.remove("active");
               'authorization': keys.authorization,
               
             }
-          }).then(res => { console.log(res.data);  setMainData(res.data) }); 
+          }).then(res => { 
+            
+            res.data.forEach(data=>{
+              data.all_topics.forEach(topic=>{
+                topic.all_series.sort((a, b) => {
+                  const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+                  const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+                  if (nameA < nameB) {
+                    return -1;
+                  }
+                  if (nameA > nameB) {
+                    return 1;
+                  }
+                  // names must be equal
+                  return 0;
+                })
+                
+              })
+
+
+            })
+            setMainData(res.data) }); 
       
         
         },[]) 
